f0c375b54c2ee212632ace493db32053078e923c
LoveNui <yujinxian40@gmail.com>
2024-01-24 09:50:57 +0000

    update: add products, Readme.MD

--- shopify_add_products.py
+++ shopify_add_products.py
@@ -9,16 +9,12 @@ SHOP_NAME = os.getenv('SHOP_NAME')
 ADMIN_API_ACCESS_TOKEN = os.getenv('ADMIN_API_ACCESS_TOKEN')
 NEW_CHECKED_PRODUCT = os.getenv('NEW_CHECKED_PRODUCT')
 
-headers = {
-    'Content-Type': 'application/json',
-    'X-Shopify-Access-Token': ADMIN_API_ACCESS_TOKEN
-}
-
 session = shopify.Session(
             f'{SHOP_NAME}.myshopify.com',
             "2023-04",
             ADMIN_API_ACCESS_TOKEN
         )
+shopify.ShopifyResource.activate_session(session)
 
 def upload_new_products(new_products):
     print("Add New Projects")
@@ -30,15 +26,14 @@ def upload_new_products(new_products):
         new_product.body_html = product["title"]
         new_product.title = product["title"]
         new_product.vendor = "OEM PART"
-        variant = shopify.Variant(
-            {
-                'price': product["Price"] if "Price" in product else 0, 
-                'sku':'', 
-                "grams": product["weight"]*1000 if "weight" in product else 0,
-                "weight": product["weight"] if "weight" in product else 0,
-                "weight_unit": "kg"
-                }
-            )
+        # set variant
+        variant = shopify.Variant()
+        variant.price = product["price"] if "price" in product else 0
+        variant.sku = ''
+        variant.grams = product["weight"]*1000 if "weight" in product else 0
+        variant.weight = product["weight"] if "weight" in product else 0
+        variant.weight_unit = "kg"
+
         new_product.variants = [variant]
         # Get the list of all pictures
         dir_list = os.listdir(os.path.join('pictures', product["title"]))
@@ -75,37 +70,34 @@ def upload_new_products(new_products):
         }
         product_metafields = []
         for y in metafield_keys:
-            # metafield = shopify.Metafield().
             if y == "Part Number":
                 if product.get(y):
                     value = "[\"" + "\",\"".join(product.get(y)) +"\"]"
-                    print(value)
-                    # product_metafields.append({"key":metafield_keys[y][0], "value":str(product.get(y)), "type":metafield_keys[y][-1], "namespace":"custom"})
-                    product_metafields.append(shopify.Metafield({"key":metafield_keys[y][0], "value":value, "type":metafield_keys[y][-1], "namespace":"custom"}))
                 else:
-                    pass
+                    value = None
             elif y == "OEM Part Number":
                 if product.get(y):
                     value = " , ".join(product.get(y))
-                    print(value)
-                    # product_metafields.append({"key":metafield_keys[y][0], "value":str(product.get(y)), "type":metafield_keys[y][-1], "namespace":"custom"})
-                    product_metafields.append(shopify.Metafield({"key":metafield_keys[y][0], "value":value, "type":metafield_keys[y][-1], "namespace":"custom"}))
                 else:
-                    pass
+                    value = None
             elif y == "Car Years range":
                 if product.get(y):
                     value = "-".join([str(i) for i in product.get(y)])
-                    # product_metafields.append({"key":metafield_keys[y][0], "value":str(product.get(y)), "type":metafield_keys[y][-1], "namespace":"custom"})
-                    product_metafields.append(shopify.Metafield({"key":metafield_keys[y][0], "value":value, "type":metafield_keys[y][-1], "namespace":"custom"}))
                 else:
-                    pass
+                    value = None
             else:
                 if product.get(y):
-                    # product_metafields.append({"key":metafield_keys[y][0], "value":product.get(y), "type":metafield_keys[y][-1], "namespace":"custom"})
-                    product_metafields.append(shopify.Metafield({"key":metafield_keys[y][0], "value":product.get(y), "type":metafield_keys[y][-1], "namespace":"custom"}))
+                    value = product.get(y)
                 else:
-                    pass
-        
+                    value = None
+            if value:
+                metafield = shopify.Metafield()
+                metafield.key = metafield_keys[y][0]
+                metafield.value = value
+                metafield.type = metafield_keys[y][-1]
+                metafield.namespace = "custom"
+                product_metafields.append(metafield)
+
         new_product.save()
         print("[Info]successfully added project")
         for i in product_metafields:
@@ -118,5 +110,5 @@ def upload_new_products(new_products):
 if __name__ == "__main__":
     with open(NEW_CHECKED_PRODUCT) as f:
         new_upload_product = json.load(f)
-
+    print(new_upload_product)
     upload_new_products(new_upload_product)
